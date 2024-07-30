18da6f7da53c057f69f553f0424b4392a7424e9f
Ivy <140263262+eyevyl@users.noreply.github.com>
2024-07-30 11:55:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document
 
     //Prepare the query
     let query5 =
-      "mutation ($myItemName: String!, $columnVals: JSON!) { create_item (board_id:6813606068, item_name:$myItemName, column_values:$columnVals) { id } }";
+      "mutation ($myItemName: String!, $columnVals: JSON!) { create_item (board_id:MY_BOARD_ID, item_name:$myItemName, column_values:$columnVals) { id } }"; //Paste your board ID here 
     let vars = {
       myItemName: "Response",
       columnVals: JSON.stringify({
@@ -34,7 +34,7 @@ document
       headers: {
         "Content-Type": "application/json",
         Authorization:
-          "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjM3MDg3ODUwNSwiYWFpIjoxMSwidWlkIjo2MjAwOTYyMCwiaWFkIjoiMjAyNC0wNi0xMVQxOTowNzozNC45MTVaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjM4OTIwMDIsInJnbiI6InVzZTEifQ.xkTLwU_pIHoUoif2H6ceVSdanLpsxwUeiTH6j9otb9k",
+          "MY_API_KEY", //Paste your Monday.com API key here
       },
       body: JSON.stringify({
         query: query5,
@@ -60,4 +60,3 @@ document
         alert("There was an error sending your responses. Please try again.");
       });
   }); //Add event listener
-// API Token: eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjM3MDg3ODUwNSwiYWFpIjoxMSwidWlkIjo2MjAwOTYyMCwiaWFkIjoiMjAyNC0wNi0xMVQxOTowNzozNC45MTVaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjM4OTIwMDIsInJnbiI6InVzZTEifQ.xkTLwU_pIHoUoif2H6ceVSdanLpsxwUeiTH6j9otb9k
